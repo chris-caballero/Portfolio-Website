4c3a772373566c2b3a61c209272e2bf6089f97f8
chris-caballero <chrismcaballero@gmail.com>
2023-08-23 14:50:44 -0400

    updated scrolling, new portfolio section. new images

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 const Header = () => {
+  const customScroll = (event, target) => {
+    event.preventDefault();
+    const targetSection = document.querySelector(target);
+    if (targetSection) {
+      const height = document.querySelector('.navbar').clientHeight;
+      const offset = targetSection.offsetTop - height;
+
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth"
+      })
+    }
+  };
+
   return (
     <header className="header-class">
       <nav className="navbar d-flex justify-content-between align-items-center">
@@ -9,11 +23,11 @@ const Header = () => {
             <a href="/">CC</a>
           </div>
           <ul className="nav-links d-flex gap-30 pt-3">
-            <li><a href="#Home">Home</a></li>
-            <li><a href="#About">About</a></li>
-            <li><a href="#Portfolio">Portfolio</a></li>
-            <li><a href="#Contact">Contact</a></li>
-            <li><a href="#Resume">Resume</a></li>
+            <li><a href="#home" onClick={(event) => customScroll(event, "#home")}>Home</a></li>
+            <li><a href="#about" onClick={(event) => customScroll(event, "#about")}>About</a></li>
+            <li><a href="#portfolio" onClick={(event) => customScroll(event, "#portfolio")}>Portfolio</a></li>
+            <li><a href="#contact" onClick={(event) => customScroll(event, "#contact")}>Contact</a></li>
+            <li><a href="/documents/resume.pdf" target="_blank">Resume</a></li>
           </ul>
         </div>
       </nav>
--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Home = () => {
+
   return (
     <>
-      <section className="title-section py-5" id="landing">
+      <section className="title-section py-5" id="home"> 
         <div className="container">
           <div className="row justify-content-center">
               <div className="col-lg-6 text-center">
@@ -16,11 +17,12 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="about-section py-5" id="landing">
+      <hr className="main-content-splitter" />
+      <section className="about-section py-5" id="about">
         <div className="container">
           <div className="row justify-content-between align-items-center">
             <div className="col-lg-4">
-              <img src="/imgs/IMG_2963.jpg" className="img-fluid" />
+              <img src="/imgs/me-2.jpg" className="img-fluid" />
             </div>
             <div className="col-lg-7 col-md-12 about-text">
               <div className="main-title">
@@ -28,28 +30,33 @@ const Home = () => {
               </div>
               <hr className="about-splitter mb-4"></hr>
               <div className="about-main-text">
-                <p>I am a machine learning engineer and an avid programmer. I studied at Florida State University where I got my B.S. in Pure Mathematics and my M.S. in Computer Science.
+                <p>I am a machine learing engineer and an avid programmer. I studied at Florida State University where I got my B.S. in Pure Mathematics and my M.S. in Computer Science.
                 </p>
                 <p>In my spare time I like to experiment with different projects, and I'm currently pursuing an MLOps specialization. Point is, I love to learn.
                 </p>
                 <p>When I'm not focused on building intelligent systems, I'm focused on getting something yummy to eat, and spending time with my cats. </p>
               </div>
-              
-
+              <ul className="about-links d-flex">
+                <li><a class="about-link-button text-uppercase" href="https://github.com/chris-caballero" target="_blank">Github</a></li>
+                <li><a class="about-link-button text-uppercase" href="https://www.linkedin.com/in/christopher-caballero-696b6b219/" target="_blank">LinkedIn</a></li>
+                <li><a class="about-link-button text-uppercase" href="/documents/resume.pdf" target="_blank">Resume</a></li>
+              </ul>
             </div>
              
           </div>
         </div>
       </section>
-      <section className="portfolio-section py-5">
+      <hr className="main-content-splitter" />
+      <section className="portfolio-section py-5" id="portfolio">
         <div className="container">
           <div className="row">
             <div className="col-lg-12 text-center">
-              <h1 className="main-title text-uppercase">Portfolio</h1>
+              <h1 className="main-title">Portfolio</h1>
+              <hr className="portfolio-splitter mb-2"></hr>
               <p style= { {'font-weight': '300' } }>My favorite projects!</p>
             </div>
           </div>
-          <div className="row justify-content-between project-item py-5">
+          <div className="row justify-content-between align-items-center project-item py-5">
               <div className="col-md-6">
                 <h2 className="mb-4">Credit Card Fraud Detection</h2>
                 <p>Trained three models to flag fraudulent transactions. Used class imbalance techniques and outlier removal to improve model performance.</p>
@@ -68,13 +75,14 @@ const Home = () => {
                 </ul>
               </div>
               <div className="col-md-4">
-                <img src="/imgs/credit-card-fraud.png" alt="Project Image" />
+                <img src="/imgs/credit-card-fraud.png" alt="Project Image" id="fraud-img" />
               </div>
           </div>          
-          <div className="row justify-content-between project-item py-5">
+          <div className="row justify-content-between align-items-center project-item py-5">
             <div className="col-md-6">
               <h2 className="mb-4">Support Ticket Classification App</h2>
-              <p>Developed a custom language model using PyTorch for classification on a support ticket dataset. Made a containerized app so you could see how your request would get classified. </p>
+              
+              <p>Developed a custom transformer language model using PyTorch for classification on a support ticket dataset. Made a user-friendly containerized Flask app so you could see how your request would get classified. </p>
               <ul className="project-info-list"> 
                 <li>
                   <span>Technologies</span>: Python, PyTorch, Docker, Flask, HTML, JS, CSS
@@ -94,12 +102,47 @@ const Home = () => {
               </ul>
             </div>
             <div className="col-md-4">
-              <img src="/imgs/support-ticket-app.png" alt="Project Image" />
+              <img src="/imgs/ticket-01.png" alt="Project Image" id="ticket-img" />
+            </div>
+          </div>
+          <div className="row justify-content-between align-items-center project-item py-5">
+            <div className="col-md-6">
+              <h2 className="mb-4">Carbon Emission Analysis</h2>
+              <p>Conducted an in-depth data analysis of carbon emissions within England, focusing on examining significant insights related to the carbon data of the Lower Supper Outer Areas (LSOAs). The dataset for this analysis was sourced from PBCC's data repository, available at <a href="https://www.carbon.place/data/">https://www.carbon.place/data/</a>.</p> 
+              <ul className="project-info-list"> 
+                <li>
+                  <span>Technologies</span>: Python, Docker, GeoPandas
+                </li>
+                <li><span>Category</span>: Classification, Language Model, Transformer</li>
+                <li><span>Date</span>: August 2023</li>
+              </ul>
+              <ul className="project-info-list project-links">
+                <li>
+                  <a href="https://github.com/chris-caballero/Carbon-Footprint-LSOA" target="_blank"><i className="fab fa-github" aria-hidden="true" />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://hub.docker.com/repository/docker/chrismcaballero/carbon-footprint-analysis/general" target="_blank"><i className="fab fa-docker" aria-hidden="true" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-4">
+              <img src="/imgs/emission-01.png" alt="Project Image" id="emission-img" />
+            </div>
+          </div>
+        </div>
+      </section>      
+      <hr className="main-content-splitter" />
+      <section className="contact-section py-5" id="contact">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h1 className="main-title">Contact</h1>
             </div>
           </div>
         </div>
       </section>
-
     </>
   );
 }
