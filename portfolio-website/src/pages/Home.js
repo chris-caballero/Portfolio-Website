d96c8a67f1432f55d82dec8ec551b36aa6cb0d49
chris-caballero <chrismcaballero@gmail.com>
2023-09-08 15:17:48 -0400

    minor index.html changes

--- portfolio-website/src/pages/Home.js
+++ portfolio-website/src/pages/Home.js
@@ -23,23 +23,23 @@ const Home = () => {
                   <p className="title-description mt-3r">Hi, hello, what's up? I love to code, and here's the result. I really hope you enjoy these projects.</p>
                 </div>
                 <div className="personal-links-container d-flex justify-content-center pt-4">
-                    <ul className="personal-links d-flex gap-30 justify-content-center">
-                      <li>
-                        <a href="https://github.com/chris-caballero" target="_blank" rel="noopener noreferrer">
-                          <i className="fab fa-github" aria-hidden="true" />
-                        </a>
-                      </li>
-                      <li>
-                        <a href="https://www.linkedin.com/in/christopher-caballero-696b6b219/" target="_blank" rel="noopener noreferrer">
-                          <i className="fab fa-linkedin" aria-hidden="true" />
-                        </a>
-                      </li>                  
-                      <li>
-                        <a href="https://www.kaggle.com/chriscaballero" target="_blank" rel="noopener noreferrer">
-                          <i id="kaggle-icon" className="fab fa-kaggle" aria-hidden="true" />
-                        </a>
-                      </li>
-                    </ul>
+                  <ul className="personal-links d-flex gap-30 justify-content-center">
+                    <li>
+                      <a href="https://github.com/chris-caballero" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+                        <i className="fab fa-github" aria-hidden="true" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.linkedin.com/in/christopher-caballero-696b6b219/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                        <i className="fab fa-linkedin" aria-hidden="true" />
+                      </a>
+                    </li>                  
+                    <li>
+                      <a href="https://www.kaggle.com/chriscaballero" target="_blank" rel="noopener noreferrer" aria-label="Kaggle">
+                        <i id="kaggle-icon" className="fab fa-kaggle" aria-hidden="true" />
+                      </a>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
@@ -72,6 +72,7 @@ const Home = () => {
                 "
                 src="/imgs/about_me/me-420.webp"
                 alt="Portrait"
+                aria-label="Portrait of Christopher Caballero"
                 className='about-img align-self-center'
                 preload="true"
               />
